94527941fa14b510f44ac5d50a81f1ba424e22ae
lijia <lijia@inke.cn>
2018-11-27 15:37:13 +0800

    learn kotlin range

--- app/src/main/java/com/testgit/example/MainActivity.kt
+++ app/src/main/java/com/testgit/example/MainActivity.kt
@@ -3,12 +3,16 @@ package com.testgit.example
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.text.SpannableString
 import android.util.Log
+import android.view.View
+import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        private const val TAG = "MainActivity"
+    }
     private lateinit var mTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,25 +21,33 @@ class MainActivity : AppCompatActivity() {
 
         mTextView = findViewById(R.id.text_view)
 
-        var str = "有20个小姐姐喜欢了你"
+        var gotoTestPageBtn = findViewById<Button>(R.id.btn_goto_page)
+        gotoTestPageBtn.setOnClickListener {
+            gotoTestPage()
+        }
 
-        var s = "20"
+        learnKotlinLoop()
 
-        var startIndex = str.lastIndexOf(s)
-        var endIndex = startIndex + s.length
+    }
 
-        Log.i("TAG", "onCreate startIndex: $startIndex ===endIndex:$endIndex")
+    /**
+     * 开始学习kotlin 循环
+     */
+    private fun learnKotlinLoop() {
+        Log.i(TAG, "learnKotlinLoop: ")
+        val array = arrayOf("a","b","c","d","e","f","g","h","i","j","k")
+        for (i in 1..10 step 2){
+            Log.i(TAG, "learnKotlinLoop1: $i ")
+        }
+        // 再 [2，array.size)区间遍历，并且每次增加2
+        for (i in 2 until array.size step 2){
+            Log.i(TAG, "learnKotlinLoop2: $i ")
+        }
 
-        var startS = str.substring(startIndex)
-//        var spinnerString = SpannableString()
+    }
 
-        var result = str.substring(startIndex,endIndex)
-        Log.i("TAG", "onCreate result: $result")
-
-
-        mTextView.text = result
-
-//        startActivity(Intent(MainActivity@this , TestActivity ::class.java))
+    private fun gotoTestPage () {
+        startActivity(Intent(MainActivity@this , TestActivity ::class.java))
     }
 
 
